892320489319fabd763675914c5e4cad0a41a681
adigun574 <adigunibrahim574@gmail.com>
2022-07-03 22:07:11 +0100

    added carousel

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,16 +4,43 @@
           <div class="bg-white p-2 absolute" style="width:190px; top:-25px">
             <img src="https://www.kloudcommerce.co/images/brand/colored-brand-logo.png">
           </div>
-        <h1 class="sm:text-4xl text-2xl">KLOUD COMMERCE CHALLENGE</h1>
-        <p class="font-thin">Have fun</p>
-        <!-- <p>Create</p>
-        <p>Explore</p> -->
+        <h1 class="sm:text-4xl text-2xl mb-4">KLOUD COMMERCE CHALLENGE</h1>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <p class="font-thin">Have fun</p>
+                </div>
+                <div class="carousel-item">
+                    <p class="font-thin">Create</p>
+                </div>
+                <div class="carousel-item">
+                    <p class="font-thin">Explore</p>
+                </div>
+            </div>
+
+            <ol class="carousel-indicators relative" style="top:30px">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+        </div>
       </div>
   </div>
 </template>
 
 <script>
 export default {
+    created(){
+        this.slideCarousel()
+    },
+    methods:{
+        slideCarousel(){
+            $('.carousel').carousel({
+                interval: 1000
+            })
+        }
+    }
 
 }
 </script>
--- src/components/TablePage.vue
+++ src/components/TablePage.vue
@@ -377,4 +377,5 @@ export default {
       padding:10px;
       font-size: 12px;
   }
+  
 </style>
