d97112ff77cc49ed7dc72a6182283ee1f337e1dd
adigun574 <adigunibrahim574@gmail.com>
2022-07-03 21:37:14 +0100

    added logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-      <div class="p-8 text-center bg-[#2a2a8f] text-white mb-4">
-          <!-- <div class="bg-white" style="width:100px; height:100px; overflow:hidden">
-            <img height="10px" src="https://media-exp1.licdn.com/dms/image/C4D0BAQERufUxUm21Vw/company-logo_200_200/0/1651058988660?e=1665014400&v=beta&t=5VEWBFNOxZgz2LLEues3MBgMzYuDx8D_oiFCdFNmRzE">
-          </div> -->
+      <div class="p-8 text-center bg-[#2a2a8f] text-white mb-4 relative">
+          <div class="bg-white p-2 absolute" style="width:190px; top:-25px">
+            <img src="https://www.kloudcommerce.co/images/brand/colored-brand-logo.png">
+          </div>
         <h1 class="sm:text-4xl text-2xl">KLOUD COMMERCE CHALLENGE</h1>
         <p class="font-thin">Have fun</p>
         <!-- <p>Create</p>
